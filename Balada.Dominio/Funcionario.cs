6c31f3fc39c9157ad76719e6f2fb8c76af4da938
AngeloBordin <angelombordin@gmail.com>
2023-10-06 20:53:35 -0300

    create Dominio

---
+++ Balada.Dominio/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿namespace Balada.Dominio;
+public class Funcionario
+{
+    public string Name { get; set; } 
+    public DateTime DateOfBirth { get; set; }
+    // Salário anual. Mensal = Salario / 13.3
+    public decimal Salary { get; set; }
+    public string Role { get; set; }
+    public string Departament { get; set; }
+    public int Age { get
+        {
+            DateTime now = DateTime.Now;
+            int age = now.Year - DateOfBirth.Year;
+            if (now.DayOfYear < DateOfBirth.DayOfYear) age--;
+
+            return age;
+        } 
+    }
+
+    public Funcionario(string name, DateTime dateOfBirth, string role,  decimal salary, string departament)
+    {
+        Name = name;
+        DateOfBirth = dateOfBirth;
+        Role = role;
+        Salary = salary;
+        Departament = departament;
+    }
+}
