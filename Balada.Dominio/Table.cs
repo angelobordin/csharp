491bc2fcab57426d4b393b681ed4f3c0638d10b3
AngeloBordin <angelombordin@gmail.com>
2023-10-07 11:43:45 -0300

    create Table(Mesa) class

---
+++ Balada.Dominio/Table.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Balada.Dominio
+{
+    public class Table
+    {
+        public required string Description { get; set; }
+        public int Number { get; set; }
+    }
+}
