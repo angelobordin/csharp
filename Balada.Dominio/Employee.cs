59b8be6cf42e0a8013aebc1417f8c14fc7c39282
AngeloBordin <angelombordin@gmail.com>
2023-10-07 11:42:46 -0300

    create Employee(Funcionario) class

---
+++ Balada.Dominio/Employee.cs
@@ -0,0 +1,29 @@
+﻿namespace Balada.Dominio;
+
+public class Employee : EntityBase
+{
+    public string Name { get; private set; } 
+    public DateTime DateOfBirth { get; private set; }
+    public decimal Salary { get; private set; }
+    public decimal Wage { get => Salary * 13.3m; }
+    public string Role { get; private set; }
+    public string Departament { get; private set; }
+    public int Age { get
+        {
+            DateTime now = DateTime.Now;
+            int age = now.Year - DateOfBirth.Year;
+            if (now.DayOfYear < DateOfBirth.DayOfYear) age--;
+
+            return age;
+        }
+    }
+
+    public Employee(string name, DateTime dateOfBirth, string role,  decimal salary, string departament)
+    {
+        Name = name;
+        DateOfBirth = dateOfBirth;
+        Role = role;
+        Salary = salary;
+        Departament = departament;
+    }
+}
